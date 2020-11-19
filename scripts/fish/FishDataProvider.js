6b222631a7ae0f3970b511660a832717cca3040a
sosinatolossa <sosinatolossa16@gmail.com>
2020-11-19 11:27:39 -0600

    changed variables

--- scripts/fish/FishDataProvider.js
+++ scripts/fish/FishDataProvider.js
@@ -31,43 +31,43 @@ export const useFish = () => {
 
 
 
-//this code will output a number that is divisible by 3
+//this code will output a fish that is divisible by 3
 export const mostHolyFish = () => {
     // 3, 6, 9, 12, etc... fish
     const holyFish = []
 
-    for (const number of fishCollection) {
-        if (number % 3 === 0) {
-            //console.log(number)
-            holyFish.push(number)
+    for (const fish of fishCollection) {
+        if (fish.length % 3 === 0) {
+            //console.log(fish)
+            holyFish.push(fish)
         }
     }
 
     return holyFish
 }
-//this code will output a number that is divisible by 5
+//this code will output a fish that is divisible by 5
 export const soldierFish = () => {
     const soldiers = []
 
     // 5, 10, 15, 20, 25, etc... fish
-    for (const number of fishCollection) {
-        if (number % 5 === 0) {
-            //console.log(number)
-            soldiers.push(number)
+    for (const fish of fishCollection) {
+        if (fish.lenght % 5 === 0) {
+            //console.log(fish)
+            soldiers.push(fish)
         }
     }
     return soldiers
 }
 
-//this code will output a number that is not divisible by 3 0r 5
+//this code will output a fish that is not divisible by 3 0r 5
 export const nonHolyFish = () => {
     const regularFish = []
 
     // Any fish not a multiple of 3 and 5
-    for (const number of fishCollection) {
-        if (number % 5 !== 0 && number % 5 !== 0) {
-            //console.log(number)
-            regularFish.push(number)
+    for (const fish of fishCollection) {
+        if (fish.length % 5 !== 0 && fish.length % 5 !== 0) {
+            //console.log(fish)
+            regularFish.push(fish)
         }
     }
     return regularFish
